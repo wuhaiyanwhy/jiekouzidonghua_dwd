d23fc6828da4fb45d07cdc62d11c3f6b9c680d82
zuohuajian <1174837652@qq.com>
2019-07-08 15:21:56 +0800

    接入返一半

--- middlegroudInterfaceTest/src/main/java/com/duoweidu/cases/message/Build.java
+++ middlegroudInterfaceTest/src/main/java/com/duoweidu/cases/message/Build.java
@@ -27,28 +27,28 @@ public class Build {
                         SqlDetail.getParamValue(0, "mobile2"),
                         SqlDetail.getParamValue(0, "mobile3"));
                 System.out.println("钉钉消息发送成功");
-            }
-        }
 
-        //当连续2次构建失败并且最近10次未发送短信的时候发送短信
-        //如果表里有数据再走此逻辑
-        if (SqlDetail.getBuildEnabled().size() >0 && SqlDetail.getBuildMessageStatus().size() > 0) {
-            if (SqlDetail.getBuildEnabled().get(0) == 0 && enabled == 0 &&
-                    SqlDetail.getBuildMessageStatus().toString().equals("[0, 0, 0, 0, 0, 0, 0, 0, 0, 0]")) {
-                message_status = 1;
-                Message.sendMessage("接口已经多次报错了，快去钉钉查看测试报告！！！",
-                        SqlDetail.getParamValue(0, "mobile1"),
-                        SqlDetail.getParamValue(0, "appId"));
-                Message.sendMessage("接口已经多次报错了，快去钉钉查看测试报告！！！",
-                        SqlDetail.getParamValue(0, "mobile2"),
-                        SqlDetail.getParamValue(0, "appId"));
-                Message.sendMessage("接口已经多次报错了，快去钉钉查看测试报告！！！",
-                        SqlDetail.getParamValue(0, "mobile3"),
-                        SqlDetail.getParamValue(0, "appId"));
-                System.out.println("短信发送成功");
+                //当连续2次构建失败并且最近10次未发送短信的时候发送短信
+                //如果表里有数据再走此逻辑
+                if (SqlDetail.getBuildEnabled().size() >0 && SqlDetail.getBuildMessageStatus().size() > 0) {
+                    if (SqlDetail.getBuildEnabled().get(0) == 0 &&
+                            SqlDetail.getBuildMessageStatus().toString().equals("[0, 0, 0, 0, 0, 0, 0, 0, 0, 0]")) {
+                        message_status = 1;
+                        Message.sendMessage("接口已经多次报错了，快去钉钉查看测试报告！！！",
+                                SqlDetail.getParamValue(0, "mobile1"),
+                                SqlDetail.getParamValue(0, "appId"));
+                        Message.sendMessage("接口已经多次报错了，快去钉钉查看测试报告！！！",
+                                SqlDetail.getParamValue(0, "mobile2"),
+                                SqlDetail.getParamValue(0, "appId"));
+                        Message.sendMessage("接口已经多次报错了，快去钉钉查看测试报告！！！",
+                                SqlDetail.getParamValue(0, "mobile3"),
+                                SqlDetail.getParamValue(0, "appId"));
+                        System.out.println("短信发送成功");
+                    }
+                }
             }
-        }
 
+        }
         //插入构建状态
         SqlDetail.insertBuild(enabled, message_status);
 
