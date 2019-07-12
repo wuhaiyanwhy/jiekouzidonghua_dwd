ec5123f50af918bf4e3dcac87f37a6bc81942792
zuohuajian <1174837652@qq.com>
2019-07-12 16:07:01 +0800

    兼容aaData

--- middlegroudInterfaceTest/src/main/java/com/duoweidu/cases/hsq/opadmin/activity_goods_index_test.java
+++ middlegroudInterfaceTest/src/main/java/com/duoweidu/cases/hsq/opadmin/activity_goods_index_test.java
@@ -1,43 +1,14 @@
 package com.duoweidu.cases.hsq.opadmin;
 
-import com.duoweidu.config.generalAssert.GeneralAssert;
-import com.duoweidu.config.TestConfigOpadmin;
-import org.apache.http.HttpResponse;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.util.EntityUtils;
+import com.duoweidu.cases.interfaces.HsqOpadminInterfaceTest;
 import org.testng.annotations.Test;
 
-import java.io.IOException;
-
-public class activity_goods_index_test {
-
-    //用来储存参数信息
-    private String param;
-    //用来储存返回结果
-    private String result;
+public class activity_goods_index_test extends HsqOpadminInterfaceTest {
 
     @Test(dependsOnGroups = "loginTrue",description = "商品列表排序管理")
-    public void activity_goods_index_true() throws IOException {
+    public void activity_goods_index_true() {
+        setUrl("activity.goods.index.uri");
+        process(true, false);
 
-        System.out.println(TestConfigOpadmin.activity_goods_index);
-
-        String results = getJsonResult();
-
-
-    }
-
-    private String getJsonResult() throws IOException {
-
-        HttpGet get = new HttpGet(TestConfigOpadmin.activity_goods_index);
-        TestConfigOpadmin.defaultHttpClient.setCookieStore(TestConfigOpadmin.store);
-        HttpResponse response = TestConfigOpadmin.defaultHttpClient.execute(get);
-
-        GeneralAssert.codeTest(response,TestConfigOpadmin.activity_goods_index,param);
-
-        result = EntityUtils.toString(response.getEntity(),"utf-8");
-        System.out.println("接口返回： " + result);
-
-        GeneralAssert.resultTest(TestConfigOpadmin.activity_goods_index,param,result);
-        return result;
     }
 }
--- middlegroudInterfaceTest/src/main/java/com/duoweidu/cases/interfaces/HsqOpadminInterfaceTest.java
+++ middlegroudInterfaceTest/src/main/java/com/duoweidu/cases/interfaces/HsqOpadminInterfaceTest.java
@@ -3,12 +3,15 @@ package com.duoweidu.cases.interfaces;
 import com.duoweidu.config.generalAssert.GeneralAssert;
 import com.duoweidu.config.generalAssert.GeneralAssertMultiChannel1;
 import com.duoweidu.config.SqlGeneral;
+import com.duoweidu.utils.CallbackInterface;
 import com.duoweidu.utils.ConfigFileUrl;
+import org.apache.http.NameValuePair;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class HsqOpadminInterfaceTest extends InterfaceTest {
 
@@ -18,6 +21,47 @@ public class HsqOpadminInterfaceTest extends InterfaceTest {
         url = ConfigFileUrl.getUrlByKey(key, ConfigFileUrl.getChannel1());
         pathId = SqlGeneral.getInterfacePathValue(ConfigFileUrl.getChannel1(), key).getId();
     }
+
+    //get请求,type=1 ==> data, type=2 ==>aaData
+    protected void process(int type, boolean isAssert, boolean isList) {
+        System.out.println(url);
+        result = CallbackInterface.getStringResult(url, pathId, this.param);
+        if (isAssert == true) {
+            generalAssertTest(type, isList);
+        }
+    }
+
+    //post请求,type=1 ==> data, type=2 ==>aaData
+    protected void process(List<NameValuePair> list, int type, boolean isAssert, boolean isList) {
+        System.out.println(url);
+        param = list.toString();
+        result = CallbackInterface.postStringResult(url, pathId, list);
+        if (isAssert == true) {
+            generalAssertTest(type, isList);
+        }
+    }
+
+    //type=1 ==> data, type=2 ==>aaData
+    protected void generalAssertTest(int type, boolean isList) {
+        try {
+            JSONObject jsonObject = new JSONObject(result);
+            if (type == 1) {
+                JSONObject data = (JSONObject) jsonObject.get("data");
+                GeneralAssert.errnoAssert(jsonObject.get("errno").toString(), jsonObject.get("errmsg").toString(), url, pathId, param, result);
+                GeneralAssert.dataAssert(jsonObject.get("data").toString(), url, pathId, param, result);
+                if (isList == true) {
+                    GeneralAssertMultiChannel1.listAssert((JSONArray) data.get("list"), url, pathId, param, result);
+                }
+            }else if (type == 2) {
+                JSONArray aaData = (JSONArray) jsonObject.get("aaData");
+                GeneralAssert.aaDataTest(aaData, url, pathId, param, result);
+            }
+        }catch (JSONException e){
+            GeneralAssertMultiChannel1.jsonAssert(url, pathId, param, result, e);
+        }
+
+    }
+
 
     //通用断言判断，只判断errno
     @Override
@@ -30,14 +74,14 @@ public class HsqOpadminInterfaceTest extends InterfaceTest {
         }
     }
 
-    //通用断言判断
+
     @Override
     protected void generalAssertTest(boolean isList) {
         try {
             JSONObject jsonObject = new JSONObject(result);
             JSONObject data = (JSONObject) jsonObject.get("data");
-            GeneralAssert.errnoAssert(jsonObject.get("errno").toString(), jsonObject.get("errmsg").toString(), url, pathId, param, result);
-            GeneralAssert.dataAssert(jsonObject.get("data").toString(), url, pathId, param, result);
+            GeneralAssertMultiChannel1.errnoAssert(jsonObject.get("errno").toString(), jsonObject.get("errmsg").toString(), url, pathId, param, result);
+            GeneralAssertMultiChannel1.dataAssert(jsonObject.get("data").toString(), url, pathId, param, result);
             if (isList == true) {
                 GeneralAssertMultiChannel1.listAssert((JSONArray) data.get("list"), url, pathId, param, result);
             }
