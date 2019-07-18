c21149b17d97131421629c72c71f203129eb8e5b
dwd <dwd@dwds-MacBook-Pro.local>
2019-07-18 19:31:55 +0800

    积分老专题接口提交

--- middlegroudInterfaceTest/src/main/java/com/duoweidu/cases/hsq/openapi/point_topicinfo_test.java
+++ middlegroudInterfaceTest/src/main/java/com/duoweidu/cases/hsq/openapi/point_topicinfo_test.java
@@ -2,15 +2,37 @@ package com.duoweidu.cases.hsq.openapi;
 
 import com.duoweidu.cases.interfaces.HsqInterfaceTest;
 import com.duoweidu.config.sql.SqlDetail;
+import com.duoweidu.model.hsq.PointTopicinfoData;
 import org.testng.annotations.Test;
 
 public class point_topicinfo_test extends HsqInterfaceTest {
+
+    PointTopicinfoData model;
 
     @Test(description = "获取积分专题sku信息")
     public void point_topicinfo_true() {
         setUrl("point.topicinfo.uri");
         param = "pointChannelId=1&topicCode=" + SqlDetail.getInstance().getParamValue("topicCode");
-        process(true,false);
+        process(true, false);
+        model = sparseJson(PointTopicinfoData.class);
+        detailAssert();
     }
 
+    private void detailAssert() {
+
+        detailAssertTest("id", model.id);
+        detailAssertTest("code", model.code);
+        detailAssertTest("title", model.title);
+        detailAssertTest("is_rtf", model.is_rtf);
+        detailAssertTest("status", model.status);
+        detailAssertTest("created_at", model.created_at);
+        detailAssertTest("updated_at", model.updated_at);
+        detailAssertTest("source_type", model.source_type);
+        detailAssertTest("last_update", model.last_update);
+        detailAssertTest("type", model.type);
+        detailAssertTest("topic_type", model.topic_type);
+        detailAssertTest("topic_sub_type", model.topic_sub_type);
+//        detailAssertTest("detail", model.detail);
+
+    }
 }
