b63091386bae145cc784a8abd37f7d455c555697
zuohuajian <1174837652@qq.com>
2019-07-23 11:06:33 +0800

    觅食蜂管理平台搭建

---
+++ middlegroudInterfaceTest/src/main/java/com/duoweidu/cases/interfaces/MsfOpadminInterfaceTest.java
@@ -0,0 +1,35 @@
+package com.duoweidu.cases.interfaces;
+
+import com.duoweidu.config.sql.SqlDetail;
+import com.duoweidu.utils.CallbackInterface;
+import org.apache.http.NameValuePair;
+import org.apache.http.message.BasicNameValuePair;
+
+import java.util.List;
+
+public class MsfOpadminInterfaceTest extends InterfaceTest {
+
+    //get请求
+    @Override
+    protected void process() {
+        System.out.println(url);
+        //通用参数
+        String par = "token=" + SqlDetail.getInstance().getParamValue(0, "token");
+        if (param != null) {
+            this.param = par + "&" + param;
+        }else {
+            this.param = par;
+        }
+        result = CallbackInterface.getStringResult(url, pathId, this.param);
+    }
+
+    //post请求
+    @Override
+    protected void process(List<NameValuePair> list) {
+        System.out.println(url);
+        //通用参数
+        list.add(new BasicNameValuePair("token", SqlDetail.getInstance().getParamValue(0, "token")));
+        param = list.toString();
+        result = CallbackInterface.postStringResult(url, pathId, list);
+    }
+}
