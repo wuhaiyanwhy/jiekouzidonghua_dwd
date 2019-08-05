12e93f41495a6812c11d8827775f5d6ee4bbbeff
zuohuajian <1174837652@qq.com>
2019-08-05 18:08:00 +0800

    增加新专题接口

--- middlegroudInterfaceTest/src/main/java/com/duoweidu/cases/hsq/openapi/coupon_couponskulist_test.java
+++ middlegroudInterfaceTest/src/main/java/com/duoweidu/cases/hsq/openapi/coupon_couponskulist_test.java
@@ -3,12 +3,15 @@ package com.duoweidu.cases.hsq.openapi;
 import com.duoweidu.cases.interfaces.HsqInterfaceTest;
 import org.testng.annotations.Test;
 
+/**
+ * 单品优惠券暂时无sku,暂时无需通用断言
+ */
 public class coupon_couponskulist_test extends HsqInterfaceTest {
 
     @Test(dependsOnGroups = "loginTrue",description = "获取单品优惠券sku列表")
     public void coupon_couponskulist_true() {
         setUrl("coupon.couponskulist.uri");
-        param = "couponId=2018098";
+        param = "couponId=2969";
         process(true,false);
     }
 
--- middlegroudInterfaceTest/src/main/java/com/duoweidu/cases/hsq/openapi/market_newskulist_test.java
+++ middlegroudInterfaceTest/src/main/java/com/duoweidu/cases/hsq/openapi/market_newskulist_test.java
@@ -1,13 +1,50 @@
 package com.duoweidu.cases.hsq.openapi;
 
 import com.duoweidu.cases.interfaces.HsqInterfaceTest;
+import com.duoweidu.model.hsq.MarketNewskulistData;
 import org.testng.annotations.Test;
 
 public class market_newskulist_test extends HsqInterfaceTest {
+
+    private MarketNewskulistData model;
 
     @Test(dependsOnGroups = "loginTrue",description ="今日新品")
     public void market_newskulist_true() {
         setUrl("market.newskulist.uri");
         process(true,true);
+        model = sparseJson(MarketNewskulistData.class);
+        detailAssert();
+    }
+
+    private void detailAssert() {
+        for (int i = 0; i < model.list.size(); i++) {
+            detailAssertTest("pinActivitiesId", model.list.get(i).pinActivitiesId);
+            detailAssertTest("skuId", model.list.get(i).skuId);
+            detailAssertTest("coupleTitle", model.list.get(i).coupleTitle);
+            detailAssertTest("couplePrice", model.list.get(i).couplePrice);
+            detailAssertTest("productId", model.list.get(i).productId);
+            detailAssertTest("marketPrice", model.list.get(i).marketPrice);
+            detailAssertTest("expiredDate", model.list.get(i).expiredDate);
+            detailAssertTest("skuPic", model.list.get(i).skuPic);
+            detailAssertTest("countLimit", model.list.get(i).countLimit);
+            detailAssertTest("endTime", model.list.get(i).endTime);
+            detailAssertTest("showCountDownLimit", model.list.get(i).showCountDownLimit);
+            detailAssertTest("canDelivery", model.list.get(i).canDelivery);
+            detailAssertTest("merchant_id", model.list.get(i).merchant_id);
+            detailAssertTest("merchant_type", model.list.get(i).merchant_type);
+            detailAssertTest("left_stock", model.list.get(i).left_stock);
+            detailAssertTest("all_stock", model.list.get(i).all_stock);
+            if (model.list.get(i).tags.size() > 0) {
+                detailAssertTest("tags", model.list.get(i).tags);
+                for (int j = 0; j < model.list.get(i).tags.size(); j++) {
+                    detailAssertTest("list.get(i).tags.get(j).text", model.list.get(i).tags.get(j).text);
+                    detailAssertTest("list.get(i).tags.get(j).background", model.list.get(i).tags.get(j).background);
+                }
+            }
+            detailAssertTest("canBought", model.list.get(i).canBought);
+            detailAssertTest("onLine", model.list.get(i).onLine);
+            detailAssertTest("inStock", model.list.get(i).inStock);
+
+        }
     }
 }
