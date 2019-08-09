dbc9a10701edc3ebd8022db62722472d8383392b
zuohuajian <1174837652@qq.com>
2019-08-09 19:21:31 +0800

    增加详细断言-u

--- middlegroudInterfaceTest/src/main/java/com/duoweidu/cases/hsq/openapi/user_clearcart_test.java
+++ middlegroudInterfaceTest/src/main/java/com/duoweidu/cases/hsq/openapi/user_clearcart_test.java
@@ -1,6 +1,7 @@
 package com.duoweidu.cases.hsq.openapi;
 
 import com.duoweidu.cases.interfaces.HsqInterfaceTest;
+import com.duoweidu.model.hsq.ResData;
 import org.apache.http.NameValuePair;
 import org.testng.annotations.Test;
 
@@ -9,11 +10,19 @@ import java.util.List;
 
 public class user_clearcart_test extends HsqInterfaceTest {
 
+    private ResData model;
+
     @Test(dependsOnGroups = "loginTrue",description = "清空购物车")
     public void user_clearcart_true() {
         setUrl("user.clearcart.uri");
         List<NameValuePair> list = new LinkedList<>();
         process(list,true,false);
+        model = sparseJson(ResData.class);
+        detailAssert();
+    }
+
+    private void detailAssert() {
+        detailAssertTest(true, "res", model.res);
     }
 
 }
---
+++ middlegroudInterfaceTest/src/main/java/com/duoweidu/model/hsq/UserBindpushclientData.java
@@ -0,0 +1,10 @@
+package com.duoweidu.model.hsq;
+
+import java.util.ArrayList;
+
+public class UserBindpushclientData {
+
+    public String clientId;
+    public String alias;
+    public ArrayList<String> tags;
+}
