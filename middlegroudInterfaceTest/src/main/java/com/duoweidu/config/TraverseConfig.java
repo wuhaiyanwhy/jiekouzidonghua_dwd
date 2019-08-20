6ac39b2e8cdd663842b36b5ca6a5d047f44305d4
zuohuajian <1174837652@qq.com>
2019-08-20 10:36:19 +0800

    增加根据model遍历数据功能

--- middlegroudInterfaceTest/src/main/java/com/duoweidu/config/TraverseConfig.java
+++ middlegroudInterfaceTest/src/main/java/com/duoweidu/config/TraverseConfig.java
@@ -1,64 +1,42 @@
-package com.duoweidu.cases.fyb.openapi;
-
-import com.duoweidu.cases.interfaces.FybInterfaceTest;
-import com.duoweidu.model.fyb.CommonGuesslikeproductsModel;
-import org.testng.annotations.Test;
+package com.duoweidu.config;
 
 import java.lang.reflect.Field;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
-public class CommonGuesslikeproducts extends FybInterfaceTest {
+/**
+ * 根据model遍历数据
+ */
+public class TraverseConfig {
 
-    private CommonGuesslikeproductsModel model;
-
-    @Test(description = "获取商品列表")
-    public void commonGuesslikeproducts() {
-        setUrl("common.guesslikeproducts.uri");
-        param = "needPagination=1&pageNum=1&pageLimit=10";
-        process(true,true);
-        model = sparseJson(CommonGuesslikeproductsModel.class);
-        detailAssert();
-
-    }
-
-    private void detailAssert() {
-        traverse(model.list);
-        Map<String, Object> map = traverseObj(model);
-        for (Map.Entry<String, Object> entry : map.entrySet()) {
-            System.out.println("key = " + entry.getKey() + ", value = " + entry.getValue());
-            assertNotNull(entry.getKey(), entry.getValue());
-        }
-
-    }
-
-    private void traverse(Object obj){
+    private Map<String, Object> traverse(Object obj){
+        Map<String, Object> map = new HashMap<>();
         if (obj instanceof ArrayList) {
             ArrayList<?> list = (ArrayList<?>) obj;
             for (Object item : list) {
-                traverseObj(item);
+                map = traverseObj(item);
             }
-            return;
+            return map;
         } else if (obj instanceof HashMap) {
-            HashMap map = (HashMap) obj;
-            for (Object key : map.keySet()) {
-                Object value = map.get(key);
-                traverseObj(value);
+            HashMap map1 = (HashMap) obj;
+            for (Object key : map1.keySet()) {
+                Object value = map1.get(key);
+                map = traverseObj(value);
             }
-            return;
+            return map;
         }
         traverseObj(obj);
 
+        return map;
     }
 
     private Map<String, Object> traverseObj(Object obj) {
-        Map<String, Object> map = new HashMap<String, Object>();
+        Map<String, Object> map = new HashMap<>();
         Field[] fields = obj.getClass().getDeclaredFields();
         for(int i = 0 , len = fields.length; i < len; i++) {
             // 对于每个属性，获取属性名
             String varName = fields[i].getName();
-//            System.out.println(varName);
             try {
                 // 获取原来的访问控制权限
                 boolean accessFlag = fields[i].isAccessible();
@@ -68,7 +46,6 @@ public class CommonGuesslikeproducts extends FybInterfaceTest {
                 Object o;
                 try {
                     o = fields[i].get(obj);
-//                    System.out.println("传入的对象中包含一个如下的变量：" + varName + " = " + o);
                     map.put(varName, o);
                 } catch (IllegalAccessException e) {
                     // TODO Auto-generated catch block
