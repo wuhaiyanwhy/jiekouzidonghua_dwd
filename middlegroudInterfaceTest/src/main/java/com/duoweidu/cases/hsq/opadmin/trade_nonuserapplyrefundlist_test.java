61734384559cc48462feacb01c3857f030ab11a6
zuohuajian <1174837652@qq.com>
2019-09-16 10:53:57 +0800

    修改未申诉订单的超时时间

--- middlegroudInterfaceTest/src/main/java/com/duoweidu/cases/hsq/opadmin/trade_nonuserapplyrefundlist_test.java
+++ middlegroudInterfaceTest/src/main/java/com/duoweidu/cases/hsq/opadmin/trade_nonuserapplyrefundlist_test.java
@@ -1,10 +1,16 @@
 package com.duoweidu.cases.hsq.opadmin;
 
 import com.duoweidu.cases.interfaces.HsqOpadminInterfaceTest;
+import com.duoweidu.utils.ConfigFileUrl;
 import org.testng.annotations.Test;
 
 
 public class trade_nonuserapplyrefundlist_test extends HsqOpadminInterfaceTest {
+
+    @Override
+    protected long limitTime() {
+        return 5000;
+    }
 
     @Test(dependsOnGroups = "loginTrue",description = "未申诉订单")
     public void trade_nonuserapplyrefundlist_true() {
