1b8883026185037a80c92f859dc087703b369fd2
zuohuajian <1174837652@qq.com>
2019-09-20 14:40:05 +0800

    增加商家优惠券

--- middlegroudInterfaceTest/src/main/java/com/duoweidu/cases/hsq/openapi/user_addskutocartv1_test.java
+++ middlegroudInterfaceTest/src/main/java/com/duoweidu/cases/hsq/openapi/user_addskutocartv1_test.java
@@ -14,31 +14,40 @@ public class user_addskutocartv1_test extends HsqInterfaceTest {
     private ResData model;
 
     List<NameValuePair> list = new LinkedList<>();
-    public void getParam(){
+    private void getParam(){
         list.add(new BasicNameValuePair("Content-Type",SqlDetail.getInstance().getParamValue(0,"Content-Type")));
         list.add(new BasicNameValuePair("skuId", SqlDetail.getInstance().getParamValue("goskuId")));
         list.add(new BasicNameValuePair("amount", SqlDetail.getInstance().getParamValue("goamount")));
     }
 
-
-    @Test(dependsOnGroups = "loginTrue", description = "加入购物车", groups = "addskutocart")
-    public void user_addskutocartv1_true(){
+    private void getRequest(List<NameValuePair> list) {
         setUrl("user.addskutocartv1.uri");
-        getParam();
-        list.add(new BasicNameValuePair("type", SqlDetail.getInstance().getParamValue("gotype")));
         process(list,true,false);
         model = sparseJson(ResData.class);
         detailAssert();
     }
 
+
+    @Test(dependsOnGroups = "loginTrue", description = "加入购物车", groups = "addskutocart")
+    public void user_addskutocartv1_true(){
+        getParam();
+        list.add(new BasicNameValuePair("type", SqlDetail.getInstance().getParamValue("gotype")));
+        getRequest(list);
+    }
+
     @Test(dependsOnGroups = "loginTrue", description = "加入购物车")
     public void user_addskutocartv1_true2(){
-        setUrl("user.addskutocartv1.uri");
         getParam();
         list.add(new BasicNameValuePair("type", SqlDetail.getInstance().getParamValue("gotype2")));
-        process(list,true,false);
-        model = sparseJson(ResData.class);
-        detailAssert();
+        getRequest(list);
+    }
+
+    @Test(dependsOnGroups = "loginTrue", description = "加入购物车（只有商家优惠）", groups = "addskutocartMerchant")
+    public void userAddskutocartv1Merchant() {
+        list.add(new BasicNameValuePair("Content-Type",SqlDetail.getInstance().getParamValue(0,"Content-Type")));
+        list.add(new BasicNameValuePair("skuId", SqlDetail.getInstance().getParamValue("merchantSkuId")));
+        list.add(new BasicNameValuePair("amount", "2"));
+        getRequest(list);
     }
 
     private void detailAssert() {
